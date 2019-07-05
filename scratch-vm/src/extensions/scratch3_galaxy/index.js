d634c20e1f0947bc3d6fe1ec14ffdef3f39fa1de
sbenateau <sbenateau@mailodie.fr>
2019-07-05 17:44:28 +0200

    first commit

---
+++ scratch-vm/src/extensions/scratch3_galaxy/index.js
@@ -0,0 +1,39 @@
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const Cast = require('../../util/cast');
+const log = require('../../util/log');
+
+class Scratch3galaxy {
+    constructor (runtime) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            id: 'galaxy',
+            name: 'galaxy',
+            blocks: [
+                {
+                    opcode: 'writeLog',
+                    blockType: BlockType.COMMAND,
+                    text: 'log [TEXT]',
+                    arguments: {
+                        TEXT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    writeLog (args) {
+        const text = Cast.toString(args.TEXT);
+        log.log(text);
+    }
+}
+
+module.exports = Scratch3galaxy;
